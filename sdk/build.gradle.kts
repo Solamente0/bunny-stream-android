ef4c071fc93ac4a37a40293bf127ca23b2317c4d
Esed Alihodzic <esed.alihodzic@povio.com>
2023-10-16 06:42:54 +0200

    Setup detekt

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 android {
@@ -30,6 +31,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+detekt {
+    config.from("$rootDir/sdk/detekt.yml")
 }
 
 dependencies {
