d99fbeccc1bebced1e7b77d1680e0c73cf9b2398
Esed Alihodzic <esed.alihodzic@povio.com>
2025-03-09 16:09:23 +0100

    Update deprecations

--- app/src/main/java/net/bunnystream/android/library/LibraryScreen.kt
+++ app/src/main/java/net/bunnystream/android/library/LibraryScreen.kt
@@ -44,11 +44,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
